4ff166309099cec0aa78638aafcb8140dddfebc6
Acker <ggaoshanxun@gmail.com>
2021-05-06 16:56:51 +0800

    added past contests

--- Aio-f/src/app/contests/past/past.component.ts
+++ Aio-f/src/app/contests/past/past.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContestsService } from '../contests.service';
 
 @Component({
 	selector: 'app-contests-past',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PastComponent implements OnInit {
 
-	constructor() { }
+	which: string = 'contests';
+	others: string = 'recent';
+	loading: boolean;
+	contests: Array<any>;
+	p: number;
+	total: number;
+
+	constructor(private contestsService: ContestsService) {
+	}
 
 	ngOnInit(): void {
 	}
 
+	setContests(data: any): void {
+		this.contests = data.contests;
+		this.contests.map(contest => {
+			let start_day = new Date(contest.start_time).getDay();
+			let end_day = new Date(contest.end_time).getDay();
+			contest.days = end_day - start_day;
+			return contest;
+		});
+		this.total = data.total;
+	}
+
+	setLoading(loading: boolean): void {
+		this.loading = loading;
+	}
+
+	getPage(page: number): void {
+		this.contestsService.getPage(page)
+			.subscribe(data => {
+				this.contests = data.contests;
+				this.total = data.total;
+				this.p = page;
+			});
+	}
 }
