b8dbe0aff179849603b136ea3c37b35b71e117ff
Acker <ggaoshanxun@gmail.com>
2021-05-07 23:02:55 +0800

    added ugly comments front-end

--- Aio-f/src/app/discussion/discussion.component.ts
+++ Aio-f/src/app/discussion/discussion.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
+import { DiscussionService } from './discussion.service';
+
+interface Tree {
+	root: TreeNode;
+}
+ 
+interface TreeNode {
+	label: string;
+	children: TreeNode[];
+}
 
 @Component({
 	selector: 'app-discussion',
@@ -7,15 +18,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscussionComponent implements OnInit {
 
-	collection = [];
-	p = 1;
-	constructor() {
-		for (let i = 1; i <= 100; i++) {
-			this.collection.push(`item ${i}`);
+	public data: Tree;
+	public selectedTreeNode: TreeNode | null;
+	description: string;
+	comments: any;
+
+	constructor( private discussionService: DiscussionService) {
+		this.selectedTreeNode = null;
+		this.data = {
+			root: {
+				label: "first",
+				children: [
+					{
+						label: "second-a",
+						children: [
+							{
+								label: "third-first",
+								children: [
+									{
+										label: "ferth",
+										children: [
+											{
+												label: "fiver",
+												children: []
+											}
+										]
+									}
+								]
+							}
+						]
+					},
+					{
+						label: "second-b",
+						children: [
+							{
+								label: "third",
+								children: []
+							}
+						]
+					}
+				]
+			}
 		}
+		console.log(this.data.root);
 	}
 
+	public selectNode( node: TreeNode ) : void {
+ 
+		this.selectedTreeNode = node;
+ 
+		/*
+		console.group( "Selected Tree Node" );
+		console.log( "Label:", node.label );
+		console.log( "Children:", node.children.length );
+		console.groupEnd();
+		*/
+ 
+	}
 	ngOnInit(): void {
+		this.discussionService.getComments()
+		.subscribe(comments => {
+			this.comments = comments[0];
+			console.log(this.comments);
+		});
 	}
 
 }
--- Aio-f/src/app/discussion/discussion.service.ts
+++ Aio-f/src/app/discussion/discussion.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators'; 
+import { Angular2TokenService } from 'angular2-token';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DiscussionService {
 
-  constructor() { }
+	constructor(private tokenService: Angular2TokenService) {
+	}
+
+	getComments(): Observable<any> {
+		let url = 'comments';
+		return this.tokenService.get(url)
+			.pipe(map(res => res.json()));
+	}
 }
